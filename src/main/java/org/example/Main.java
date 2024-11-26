cca169c9610efd8ec184ce82f46f9a38868a5912
ezgi-karaca <w.ezgikaraca@gmail.com>
2024-11-26 19:58:16 +0300

    Finished

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import org.example.model.*;
+import org.example.model.enums.LampType;
+import org.example.model.enums.PaintColor;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        Wall wall1 = new Wall("Blue");
+        Wall wall2 = new Wall("White");
+        Wall wall3 = new Wall("White");
+        Wall wall4 = new Wall("Gray");
+        Ceiling ceiling = new Ceiling(15, PaintColor.GREEN);
+        Bed bed = new Bed("King Size",2,2,2,2);
+        Lamp lamp = new Lamp(LampType.LAVA, true, 2);
+        Wardrobe wardrobe = new Wardrobe(2,2,2);
+        Carpet carpet = new Carpet(2,2,PaintColor.GREEN);
+
+        Bedroom myBedroom = new Bedroom(
+                "Master Bedroom",
+                wall1, wall2, wall3, wall4,
+                ceiling, bed, lamp, wardrobe, carpet
+        );
     }
 }
